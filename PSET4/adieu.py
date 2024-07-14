92cf33ba7296c978b4ac79f758fcc89d3a79b751
Wai Yan Kyaw <40914406+WaiYanKyaw95@users.noreply.github.com>
2024-07-14 17:51:09 +0400

    Add pseudo codes for adieu.py and game.py
    I will use pseudo codes to tackle these problems.

---
+++ PSET4/adieu.py
@@ -0,0 +1,7 @@
+# prompt for user's input, one per line until the user inputs control-d
+# user will input at least one name
+# bid adieu to those names separating
+#   two names with one 'and'
+#   three name with two commas and one 'and'
+#   'n' names with 'n-1' commas and one 'and'
+# check 'inflect' module here pypi.org/project/inflect and use 'pip install inflect' to install the library
---
+++ PSET4/game.py
@@ -0,0 +1,9 @@
+# prompt the user for a level 'n'. 
+#   if the user does not input a positive integer, the program should prompt again.
+# randomly generates an integer between 1 and 'n', inclusive, using 'random' module
+# random module from docs.python.org/3/library/random.html.
+# prompt the user to guess that integer
+#   if the guess is not a positive integer, the program should prompt the user again
+#       if the guess is smaller than that integer, the program should output 'Too small!' and prompt the user again
+#       if the guess is larger than that integer, the program should output 'Too large!' and prompt the user again
+#       if the guess is the same as that integer, the program should output 'Just right!' and exit.
