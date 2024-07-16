48f399422316d235d8de7f0507f2a5fb3a808cf6
Wai Yan Kyaw <40914406+WaiYanKyaw95@users.noreply.github.com>
2024-07-17 00:20:42 +0400

    Add code to adieu.py
    Write pseudo code and Python code to tackle the adieu problem

--- PSET4/adieu.py
+++ PSET4/adieu.py
@@ -1,7 +1,28 @@
-# prompt for user's input, one per line until the user inputs control-d
-# user will input at least one name
-# bid adieu to those names separating
-#   two names with one 'and'
-#   three name with two commas and one 'and'
-#   'n' names with 'n-1' commas and one 'and'
-# check 'inflect' module here pypi.org/project/inflect and use 'pip install inflect' to install the library
+# import inflect library
+# The methods of the class engine in module inflect.py provide plural inflections,
+# singular noun inflections, “a”/”an” selection for English words, and manipulation of numbers as words.
+import inflect
+
+p = inflect.engine()
+
+# initiate the list
+nameList = []
+
+# while true
+while (True):
+# ask for user input for the name - one name per line
+# and append the new name to the list, using append() method
+    try:
+        name = input("Name: ")
+        nameList.append(name)
+# get out of the loop if user input control-d
+# by catching the EOFError error
+    except EOFError:
+        print("")
+        break
+
+# use inflect.engine().join() method to format the string
+nameList = p.join(nameList)
+
+# print out the adieu string
+print("Adieu, adieu, to " + nameList)
